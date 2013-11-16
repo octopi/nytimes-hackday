8711cd651ca60206022b20e617e9e630794fe386
David Hu <octopie@gmail.com>
2013-11-16 12:19:41 -0500

    Implement getNYTimesTrending

--- server.js
+++ server.js
@@ -1,17 +1,34 @@
 var express = require('express'),
 ig = require('instagram-node').instagram(),
 async = require('async'),
-request = require('request');
+request = require('request'),
+_ = require('underscore');
 
 var app = express();
 app.use(express.logger());
 app.use(express.bodyParser());
 
 var getNYTimesTrending = function() {
+  var nytUrl = 'http://api.nytimes.com/svc/mostpopular/v2/mostshared/all-sections/1.json?api-key=c02874f3984ca7bfbd7a35d7e91ba730:16:43844472',
+  facets = {};
+  request(nytUrl, function(err, response, body) {
+    if(!err && response.statusCode === 200) {
+      var results = JSON.parse(body).results;
+      for(var i = 0; i < results.length; i++) {
+        var currArticle = results[i];
+        _.each(currArticle.des_facet, addToFacets(currArticle.url));
+        _.each(currArticle.org_facet, addToFacets(currArticle.url));
+        _.each(currArticle.per_facet, addToFacets(currArticle.url));
+        _.each(currArticle.geo_facet, addToFacets(currArticle.url));
+      }
+      return facets;
+    }
+  });
 
-  return {
-    'USA': 'http://nytimes.com/url',
-    'thanksgiving': 'http://nytimes.com/url'
+  var addToFacets = function(url) {
+    return function(facet) {
+      facets[facet] = url;
+    };
   };
 };
 var pullFromTwitter = function() {
