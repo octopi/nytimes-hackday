1d1790c6b36889d9b39d8f9681a67f0b7f642182
Angel Say <sayangel@gmail.com>
2013-11-16 12:18:21 -0500

    initial pullFromTwitter implementation

--- server.js
+++ server.js
@@ -1,14 +1,39 @@
 var express = require('express'),
 ig = require('instagram-node').instagram(),
 async = require('async');
+var Twit = require('twit')
 
 var app = express();
 app.use(express.logger());
 app.use(express.bodyParser());
 
+var T = new Twit({
+    consumer_key:         'mQra7aCPJjI3tzqvP6Icw'
+  , consumer_secret:      'HWbc7P6jEGHQDPSOSeSBn4kJu739nMw4czmkQDueQZY'
+  , access_token:         '20206051-KNUVBKic9XyG5GVBZuVf180hLqsiI1HWUpQcGD4Bb'
+  , access_token_secret:  'AZAmGHoPFZtVvOw5yQ8A6zDD5mGe6NPZnrGFvRzYHBLHx'
+})
+
 var getNYTimesTrending = function() {};
 var pullFromTwitter = function() {
   console.log('yo');
+  var stream = T.stream('statuses/filter', { track: 'instagram com', language: 'en' })
+
+	stream.on('tweet', function (tweet) {
+		if(tweet.entities.urls.length > 0)
+		{
+			for(var x = 0; x < tweet.entities.urls.length; x++)
+			{
+				if(tweet.entities.urls[x].expanded_url.indexOf("instagram.com") !== -1)
+				{
+					console.log(tweet.text)
+					console.log(tweet.entities.urls[x].expanded_url)
+				}
+			}
+			
+		}
+  		
+	})
 };
 var makeComment = function(instagramUrl) {};
 
